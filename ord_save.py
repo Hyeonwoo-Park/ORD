7887b322d922e7b68758898b67a8b11be6c0bc1e
HyeonWoo Park <sunghakzz@naver.com>
2020-07-19 14:09:26 +0900

    echo 명령어의 newline 삽입 제거
    Signed-off-by: HyeonWoo Park <sunghakzz@naver.com>

--- ord_save.py
+++ ord_save.py
@@ -3,52 +3,56 @@ import re
 from functools import reduce
 
 def addToClipBoard(text):
-    command = 'echo ' + text.strip() + '| clip'
+    command = 'echo | set /p =' + text.strip() + '| clip'
     os.system(command)
 
 dirName = "C:/Users/{}/Documents/Warcraft III/CustomMapData/ORD9".format(os.getlogin())
 
+file = open(
+            next(
+                map(
+                    lambda x:
+                        os.path.join(dirName,x),
+                    filter(
+                        lambda x:
+                            re.search("_{}.txt".format(
+                                reduce(
+                                    lambda acc, x:
+                                        max(acc,x),
+                                    map(
+                                        int,
+                                        map(
+                                            lambda x:
+                                                re.sub(
+                                                    "[a-zA-Z0-9]*_|.txt",
+                                                    "",
+                                                    x
+                                                )
+                                            ,
+                                            os.listdir(dirName)
+                                        )
+                                    )
+                                )
+                            ),x),
+                        os.listdir(dirName)
+                    )
+                )
+            ),
+            'r',
+            encoding="UTF8"
+        )
+
 addToClipBoard(
-    "-load {}".format(
+    '-load {}'.format(
         list(
             map(
                 lambda x:
                     x.replace('"',"")
                         .split(),
-                open(
-                    next(
-                        map(
-                            lambda x:
-                                os.path.join(dirName,x),
-                            filter(
-                                lambda x:
-                                    re.search("_{}.txt".format(
-                                        reduce(
-                                            lambda acc, x:
-                                                max(acc,x),
-                                            map(
-                                                int,
-                                                map(
-                                                    lambda x:
-                                                        re.sub(
-                                                            "[a-zA-Z0-9]*_|.txt",
-                                                            "",
-                                                            x
-                                                        )
-                                                    ,
-                                                    os.listdir(dirName)
-                                                )
-                                            )
-                                        )
-                                    ),x),
-                                os.listdir(dirName)
-                            )
-                        )
-                    ),
-                    'r',
-                    encoding="UTF8"
-                )
+                file
             )
         )[5][3]
     )
 )
+
+file.close()
